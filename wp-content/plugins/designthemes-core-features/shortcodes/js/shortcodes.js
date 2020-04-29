1280cde38f6e3d57f2f743a6327e1dc1050c7026
sharath <sharath.dt@gmail.com>
2020-04-29 18:28:34 +0530

    fix old styles + preloader + remove old slider

--- wp-content/plugins/designthemes-core-features/shortcodes/js/shortcodes.js
+++ wp-content/plugins/designthemes-core-features/shortcodes/js/shortcodes.js
@@ -628,3 +628,17 @@ if ('function' !== typeof (window['vc_waypoints'])) {
 		}
 	}
 }
+
+// header slider
+jQuery(document).ready(function($) {
+		  $('#header-slider').slick({
+		  	dots: true,
+		    infinite: true,
+		    speed: 700,
+		    autoplay: true,
+		    autoplaySpeed: 2000,
+		    arrows:false,
+		    slidesToShow: 1,
+		    slidesToScroll: 1,
+		  });
+})
