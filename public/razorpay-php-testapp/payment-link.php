cc0f086bd070775e3d0fb6c9fe96e8d9d0a0e13b
sharath <sharath.dt@gmail.com>
2020-05-20 14:50:40 +0530

    add partial payment bool

--- public/razorpay-php-testapp/payment-link.php
+++ public/razorpay-php-testapp/payment-link.php
@@ -19,7 +19,7 @@ $orderData = [
     'amount'          => 19980 * 100, // 2000 rupees in paise
     'currency'        => 'INR',
     "description"       => "Engineering Guide (Premium Pack)",
-    "partial_payment"   => "1"
+    "partial_payment"   => true
 ];
 
 $razorpayOrder = $api->invoice->create($orderData);
