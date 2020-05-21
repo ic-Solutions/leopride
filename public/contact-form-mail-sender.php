1ec259aae39e66170bdb3648e5db238992150d0c
sharath <sharath.dt@gmail.com>
2020-05-21 10:33:15 +0530

    contact page form update 16

--- public/contact-form-mail-sender.php
+++ public/contact-form-mail-sender.php
@@ -45,7 +45,7 @@ if(isset($_POST['submit'])){
     $mail->isHTML(true);    
     $mail->Subject = "Contact form submitted by $name"; // This is your subject
 
-    $mail->Body = "Hi $name, <br/> We have received the following details.<br /><br />Name: $name<br />Phone: $phone<br />Email: $email<br />Message: $message<br /><br />We will get back to you as soon as possible. In case you think we missed it, please email us at consult@leopride.in<br /><br />Thanks<br />LeoPride Career Solutions<br />https://leopride.in";
+    $mail->Body = "Hi $name, <br/> We have received the following details.<br /><br />Name: $name<br />Phone: $phone<br />Email: $email<br />Message: $message<br /><br />We will get back to you as soon as possible. In case you think we missed it, please call us at 9911203280<br /><br />Thanks<br />LeoPride Career Solutions<br />https://leopride.in";
     $mail->send();
             header("Location: success.php");
         } catch (Exception $e) {
