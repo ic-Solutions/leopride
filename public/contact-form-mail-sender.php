aab053d80ada8f29bf6ab8ee435f40f4c0777bd6
sharath <sharath.dt@gmail.com>
2020-05-21 08:27:21 +0530

    contact page form update 5

--- public/contact-form-mail-sender.php
+++ public/contact-form-mail-sender.php
@@ -1,60 +1,61 @@
+
 <?php
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
+/**
+ * This example shows how to handle a simple contact form.
+ */
+
+//Import PHPMailer classes into the global namespace
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
 
-
+$msg = '';
 //Don't run this unless we're handling a form submission
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if (array_key_exists('email', $_POST)) {
+    date_default_timezone_set('Etc/UTC');
 
-    // Load Composer's autoloader
     require 'vendor/autoload.php';
 
-    // $name=$_POST["name"];
-    // $email = $_POST["email"];
-    // $phone = $_POST["phone"];
-    // $message = $_POST["message"];
-    // $to = "sharath.dt@gmail.com";
+    //Create a new PHPMailer instance
+    $mail = new PHPMailer;
+    //Tell PHPMailer to use SMTP - requires a local mail server
+    //Faster and safer than using mail()
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'sg2plcpnl0113.prod.sin2.secureserver.net';        // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'submissions@leopride.in';                     // SMTP username
+    $mail->Password   = ';a~VtLxho.5!';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-    // Instantiation and passing `true` enables exceptions
-    $mail = new PHPMailer(true);
-
-    try {
-        //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-        $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = 'sg2plcpnl0113.prod.sin2.secureserver.net';        // Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'submissions@leopride.in';                     // SMTP username
-        $mail->Password   = ';a~VtLxho.5!';                               // SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-        //Recipients
-        $mail->setFrom('consult@leopride.in', 'LeoPride Career Solutions');
-        //$mail->addAddress('sharath.dt@gmail.com', 'Shartah D T');     // Add a recipient          // Name is optional
-        $mail->addReplyTo($email);
-        $mail->addCC('sharath.dt@gmail.com');
-        //$mail->addBCC('sharathdt@gmail.com');
-
-        // Attachments
-        //$mail->addAttachment('/home/leopridevv/public_html/leopride-website/CNAME');         // Add attachments
-        //$mail->addAttachment('/php.ini');    // Optional name
-
-        // Content
-        $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = '$_POST['subject']';
-        $mail->Body    = 'Name: $_POST['name']<br />Email: $_POST['email']<br />Phone: $_POST['phone']<br />message: $_POST['message']';
-        $mail->AltBody = '$message';
-
-        $mail->send();
-        echo 'Message has been sent';
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    //Use a fixed address in your own domain as the from address
+    //**DO NOT** use the submitter's address here as it will be forgery
+    //and will cause your messages to fail SPF checks
+    $mail->setFrom('consult@leopride.in', 'LeoPride Career Solutions');
+    //Send the message to yourself, or whoever should receive contact for submissions
+    $mail->addAddress('sharath.dt@gmail.com', 'Sharath Kumar');
+    //Put the submitter's address in a reply-to header
+    //This will fail if the address provided is invalid,
+    //in which case we should ignore the whole request
+    if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
+        $mail->Subject = 'PHPMailer contact form';
+        //Keep it simple - don't use HTML
+        $mail->isHTML(false);
+        //Build a simple message body
+        $mail->Body = <<<EOT
+        Email: {$_POST['email']}
+        Name: {$_POST['name']}
+        Message: {$_POST['message']}
+        EOT;
+        //Send the message, check for errors
+        if (!$mail->send()) {
+            //The reason for failing to send will be in $mail->ErrorInfo
+            //but you shouldn't display errors to users - process the error, log it on your server.
+            $msg = 'Sorry, something went wrong. Please try again later.';
+        } else {
+            $msg = 'Message sent! Thanks for contacting us.';
+        }
+    } else {
+        $msg = 'Invalid email address, message ignored.';
     }
-
-
-
 }
+?>
